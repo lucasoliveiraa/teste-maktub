fc906e6d4237529dee5bc6f0d6f5a472b92218f1
lupandemo <lucasjackcosta18@gmail.com>
2021-02-04 16:00:32 -0300

    Authenticated

--- backend/src/modules/character/infra/http/routes/characters.routes.ts
+++ backend/src/modules/character/infra/http/routes/characters.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 import CharactersController from '../Controllers/CharactersController';
 
+import ensureAuthenticated from '../../../../../middlewares/ensureAuthenticated';
+
 const charactersRouter = Router();
 
-charactersRouter.post('/character', CharactersController.create);
-charactersRouter.get('/character/create', CharactersController.show);
-charactersRouter.get('/character/update/:id', CharactersController.update);
+charactersRouter.post(
+  '/character',
+  ensureAuthenticated,
+  CharactersController.create,
+);
+charactersRouter.get('/character/list', CharactersController.show);
+charactersRouter.get(
+  '/character/update/:id',
+  ensureAuthenticated,
+  CharactersController.update,
+);
 
 export default charactersRouter;
