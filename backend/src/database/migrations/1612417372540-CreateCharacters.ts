f7f4c01836aa0e0afb55a0e7cad8cef64a5d1d10
lupandemo <lucasjackcosta18@gmail.com>
2021-02-04 02:44:50 -0300

    database

---
+++ backend/src/database/migrations/1612417372540-CreateCharacters.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateCharacters1612417372540
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {}
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
